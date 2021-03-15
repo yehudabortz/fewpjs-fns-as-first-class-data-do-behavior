822fee8618bf6b0540de6fe56ce4fffc5c473822
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-15 16:16:45 -0500

    commit tree 6a2a3ce83c26c1075bc62ca1732c2e6588ee4d9b

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ document.querySelector("button").addEventListener("click", handleClick);
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
+  // displayMessage("hello")
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message) {
+document.querySelector('#greeting').innerText = message;
+  // alert(message);
+}
+
+function greet(timeStr) {
+  const hour = parseInt(timeStr, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
